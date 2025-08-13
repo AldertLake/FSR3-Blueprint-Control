39f944e5983480f580c5436aaccce47b542ac0d0
AldertLake <wassouilyass@icloud.com>
2025-08-13 22:15:25 +0100

    Creating Basic Plugin Structure

---
+++ Source/FSR3_Blueprint_Control/Private/FSR3_Blueprint_ControlBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FSR3_Blueprint_ControlBPLibrary.h"
+#include "FSR3_Blueprint_Control.h"
+
+UFSR3_Blueprint_ControlBPLibrary::UFSR3_Blueprint_ControlBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UFSR3_Blueprint_ControlBPLibrary::FSR3_Blueprint_ControlSampleFunction(float Param)
+{
+	return -1;
+}
+
